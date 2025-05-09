9cae8439393bc5be8bcbe0cb612f52bf4cb82352
William Banham <will_banh@icloud.com>
2025-05-09 20:56:18 +0100

    bug: hard coded map key because i cba with env anymore

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,17 +1,19 @@
 export default function GoogleMap() {
-    return (
-        <iframe
-            // width="800"
-            height="432"
-            style={{
-                border: 0
-            }}
-            className="rounded-lg shadow-md md:w-[800px] w-[300px]"
-            loading="lazy"
-            allowFullScreen
-            referrerPolicy="no-referrer-when-downgrade"
-            src={`https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_MAPS_API_KEY}
-    &q=${encodeURI("PC Clinic, Burton and South Derbyshire College Lichfield Street, Burton-on-Trent DE14 3RL")}`}>
-        </iframe>
-    )
+  return (
+    <iframe
+      // width="800"
+      height="432"
+      style={{
+        border: 0,
+      }}
+      className="rounded-lg shadow-md md:w-[800px] w-[300px]"
+      loading="lazy"
+      allowFullScreen
+      referrerPolicy="no-referrer-when-downgrade"
+      src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyD5d6IUhbCpmI1vnyj8RgrH-1VbcsE05SQ
+    &q=${encodeURI(
+      "PC Clinic, Burton and South Derbyshire College Lichfield Street, Burton-on-Trent DE14 3RL"
+    )}`}
+    ></iframe>
+  );
 }
